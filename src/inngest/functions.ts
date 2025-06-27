6aa41e22fbba8841629006596d5a432107ec124f
MnCSD <mnikolopoylos@gmail.com>
2025-06-27 14:03:30 +0300

    05: ai-jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,16 @@
 import { inngest } from "./client";
+import { Agent, openai, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
     await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+    const summarizer = createAgent({
+      name: "writer",
+      system:
+        "You are an expert writer.  You write readable, concise, simple content.",
+      model: openai({ model: "gpt-4o" }),
+    });
   }
 );
