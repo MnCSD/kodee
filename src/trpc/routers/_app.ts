507c54517543055a3eb1b0179c71e4e261692b5d
MnCSD <mnikolopoylos@gmail.com>
2025-06-28 00:16:30 +0300

    09: projects

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
+import { projectsRouter } from "@/modules/projects/server/prodedures";
 import { createTRPCRouter } from "../init";
 
 import { messagesRouter } from "@/modules/messages/server/prodedures";
 export const appRouter = createTRPCRouter({
   messages: messagesRouter,
+  projects: projectsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
