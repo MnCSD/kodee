3c45b01f7e2874e9a1bcb57bba5522055d0d6d80
MnCSD <mnikolopoylos@gmail.com>
2025-06-27 17:47:00 +0300

    07: agents-tools

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,30 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
 import Image from "next/image";
 import { useState } from "react";
+import { toast } from "sonner";
 
 export default function Home() {
   const [value, setValue] = useState("");
+  const trpc = useTRPC();
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Background job invoked successfully!");
+      },
+    })
+  );
 
   return (
     <div>
       <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button onClick={() => invoke.mutate({ value: value })}>
+        Invoke Background Job
+      </Button>
     </div>
   );
 }
